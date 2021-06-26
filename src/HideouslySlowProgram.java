92cf78212845e9a7153c7cbc17006ca65c429394
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-06-26 17:43:24 -0500

    Feat: Reasons to not write forloops like this

---
+++ src/HideouslySlowProgram.java
@@ -0,0 +1,14 @@
+public class HideouslySlowProgram {
+
+    public static void main(String[] args){
+
+
+        //this is funny because your printing the entire Long value out
+        Long sum = 0l;
+        for (long i =0 ; i < Integer.MAX_VALUE; i++){
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
