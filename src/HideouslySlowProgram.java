460fc45fc8ced79e4978e66c36e8e35362a0840d
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-06-27 02:40:12 -0500

    Update HideouslySlowProgram.java

--- src/HideouslySlowProgram.java
+++ src/HideouslySlowProgram.java
@@ -3,7 +3,7 @@ public class HideouslySlowProgram {
     public static void main(String[] args){
 
 
-        //this is funny because your printing the entire Long value out
+        //this is funny because you're printing the entire Long value out
         Long sum = 0l;
         for (long i =0 ; i < Integer.MAX_VALUE; i++){
             sum += i;
